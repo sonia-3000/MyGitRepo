99392477366def29635c1896e0f4cb2903eefcc2
sonia-3000 <sonianimley@yahoo.co.uk>
2024-12-17 21:29:35 +0000

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,30 @@
+#use casting to declare the variables (checking the notes) then convert them eg by using int(), then print all on separate lines
+
+num1 = 99.23
+num2 = 23
+num3 = 150
+string1 = "100"
+
+
+print(type(num1))
+print(type(num2))
+print(type(num3))
+print(type(string1))
+
+
+int(num1),
+float(num2),
+str(num3),
+int(string1)
+
+#I am not sure if the above is necessary because when I then tried to print it did not seem to work so I have repeated the
+#casting again
+
+print (int(num1)) 
+print (float(num2)) 
+print (str(num3)) 
+print (int(string1))
+
+
+
+
---
+++ hello_world.py
@@ -0,0 +1,13 @@
+# write an input line for the username and then another line/instruction to print it out
+
+name = input("Enter your name: ")
+print(name)
+
+age = input("Enter your age: ")
+print(age)
+
+greeting = ("Hello World!")
+print(greeting)
+
+
+
